33e38ae077d26da1e53139a0494b25a2d3a2a372
Brandon Shewmake <bshewmakedev@gmail.com>
2022-10-20 15:36:20 -0700

    [moderate] deleted default infrastructure

--- Lifestyles.Infrastructure.Default/Budget/Models/JsonBudget.cs
+++
@@ -1,9 +0,0 @@
-using Lifestyles.Infrastructure.Default.Live.Models;
-
-namespace Lifestyles.Infrastructure.Default.Budget.Models
-{
-    public class JsonBudget : JsonLifestyle
-    {
-        public int Amount { get; set; }
-    }
-}
--- Lifestyles.Infrastructure.Default/Categorize/Models/JsonCategory.cs
+++
@@ -1,7 +0,0 @@
-namespace Lifestyles.Infrastructure.Default.Categorize.Models
-{
-    public class JsonCategory
-    {
-        public string Label { get; set; } = string.Empty;
-    }
-}
